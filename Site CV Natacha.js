c570aa2b8d07a4de52b20f089104dfde58e6f23b
Natacha Desse <natacha.chaville@gmail.com>
2022-10-20 08:57:29 +0200

    menu qui se referme

--- Site CV Natacha.js
+++ Site CV Natacha.js
@@ -4,15 +4,16 @@ const burgerMenu = document.querySelector("span.material-symbols-outlined");
 const checkboxMenu = document.getElementById("mobile");
 
 //EVENT LISTENER
+
+//Ferme menu lors d'un click sur la navigation 
 for (var i=0; i < elementMenu.length; i++) {
     elementMenu[i].addEventListener('click',() => {
-
-    }
-    
-    )
+        checkboxMenu.checked = false;
+        burgerMenu.textContent = 'menu';
+    });
 }
 
-
+//Change l'icone de la checkbox en croix lorsque le menu est ouvert
 checkboxMenu.addEventListener('change', ($event) => {
     if ($event.target.checked){
         burgerMenu.textContent = 'close';

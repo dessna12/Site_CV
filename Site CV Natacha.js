47924ac68fa9b2b29e345683a561e822724ed1ce
Natacha Desse <natacha.chaville@gmail.com>
2022-10-20 08:38:16 +0200

    Ajout de la croix menu mobile

--- Site CV Natacha.js
+++ Site CV Natacha.js
@@ -1,4 +1,25 @@
 //GET ACCESS DOM
+const elementMenu = document.querySelectorAll("a.element-menu");
+const burgerMenu = document.querySelector("span.material-symbols-outlined");
+const checkboxMenu = document.getElementById("mobile");
+
+//EVENT LISTENER
+for (var i=0; i < elementMenu.length; i++) {
+    elementMenu[i].addEventListener('click',() => {
+
+    }
+    
+    )
+}
+
+
+checkboxMenu.addEventListener('change', ($event) => {
+    if ($event.target.checked){
+        burgerMenu.textContent = 'close';
+    }else{
+        burgerMenu.textContent = 'menu';
+    }
+});
 
 
 
@@ -12,9 +33,9 @@ function reveal() {
       var elementVisible = 0;
   
       if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add(".active");
+        reveals[i].classList.add("active");
       } else {
-        reveals[i].classList.remove(".active");
+        reveals[i].classList.remove("active");
       }
     }
   }
